9d66c39c5b9e1a0373c45a5a288c0ca959e74fc5
fistondesign <knfintaganda@gmail.com>
2021-12-01 16:43:01 +0200

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// $('.burger, .overlay').click(function(){
+//     $('.burger').toggleClass('clicked');
+//     $('.overlay').toggleClass('show');
+//     $('nav').toggleClass('show');
+//     $('body').toggleClass('overflow');
+//   });
+//   $(function() {
+//       $(window).on("scroll", function() {
+//           if($(window).scrollTop() > 50) {
+//               $(".burger-container").addClass("burger-bg");
+//           } else {
+//               //remove the background property so it comes transparent again (defined in your css)
+//              $(".burger-container").removeClass("burger-bg");
+//           }
+//       });
+//   });
+
